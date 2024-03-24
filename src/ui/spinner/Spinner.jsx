467c17156edc6d3ab340f6445857e9d8b59b10c5
Unni Krishnan <developer8088@gmail.com>
2024-03-24 11:54:22 +0530

    commit

--- src/ui/spinner/Spinner.jsx
+++ src/ui/spinner/Spinner.jsx
@@ -3,7 +3,7 @@ import "./spinner.scss";
 export default function Spinner() {
   return (
     <div className="spinner">
-      <img src="/public/Animation - 1701155838422 (1).gif" alt="" />
+      <img src="/Animation - 1701155838422 (1).gif" alt="" />
     </div>
   );
 }
